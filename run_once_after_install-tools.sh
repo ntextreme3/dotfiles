0b11f048eb519dfb7790ce24bfa2c380ed63e40c
Nik Trenchi <ntrenchi@bloomberg.net>
2025-01-09 15:44:45 -0500

    add tree to installs

--- run_once_after_install-tools.sh
+++ run_once_after_install-tools.sh
@@ -9,7 +9,7 @@ fi
 
 # install things with apt
 sudo apt update
-missing_apts=$(python3 _find_missing_packages.py tmux rg::ripgrep jq rustup)
+missing_apts=$(python3 _find_missing_packages.py tmux rg::ripgrep jq rustup tree)
 if [[ ! -z $missing_apts ]]; then
     sudo apt install -y $missing_apts
 fi
