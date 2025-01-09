7627a101ec457cd4730c24a2998e141bdfdf6801
Nik Trenchi <ntrenchi@bloomberg.net>
2025-01-09 15:21:57 -0500

    fix install script for when there are multiple missing packages

--- run_once_after_install-tools.sh
+++ run_once_after_install-tools.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 
 # install things with snap
 missing_snaps=$(python3 _find_missing_packages.py jqp yq)
-if [ ! -z $missing_snaps ]; then
+if [[ ! -z $missing_snaps ]]; then
     sudo snap install $missing_snaps
 fi
 
 # install things with apt
 sudo apt update
 missing_apts=$(python3 _find_missing_packages.py tmux rg::ripgrep jq rustup)
-if [ ! -z $missing_apts ]; then
+if [[ ! -z $missing_apts ]]; then
     sudo apt install -y $missing_apts
 fi
 
