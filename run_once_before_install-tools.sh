f0f5e2bd63125ff92933d96524110e301fc14868
Nik Trenchi <ntextreme3@gmail.com>
2025-01-10 10:20:03 -0500

    move run script from 'after' to 'before'

--- run_once_before_install-tools.sh
+++ run_once_before_install-tools.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+# install dependencies
+sudo apt update
+sudo apt install -y build-essential cmake
+
+# install things with apt
+missing_apts=$(python3 _find_missing_packages.py tmux rg::ripgrep jq rustup tree)
+if [[ ! -z $missing_apts ]]; then
+    sudo apt install -y $missing_apts
+fi
+
 # install things with snap
 missing_snaps=$(python3 _find_missing_packages.py jqp yq)
 if [[ ! -z $missing_snaps ]]; then
     sudo snap install $missing_snaps
-fi
-
-# install things with apt
-sudo apt update
-missing_apts=$(python3 _find_missing_packages.py tmux rg::ripgrep jq rustup tree)
-if [[ ! -z $missing_apts ]]; then
-    sudo apt install -y $missing_apts
 fi
 
 # setup rust
