94f0e9cdd9ae45e14b67fdb9fcd728c2184ccc2e
Nik <8700286+ntextreme3@users.noreply.github.com>
2025-01-16 09:00:07 -0500

    add pyenv deps

--- run_once_before_install-tools.sh
+++ run_once_before_install-tools.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 sudo apt update
 sudo apt install -y build-essential cmake
 
+# install pyenv recommended deps
+sudo apt install -y --no-upgrade build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl git \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 # install things with apt
 missing_apts=$(python3 ${CHEZMOI_SOURCE_DIR}/_find_missing_packages.py tmux rg::ripgrep jq rustup tree)
 if [[ ! -z $missing_apts ]]; then
