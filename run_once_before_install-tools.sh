4acf38bbdb2d8a1fbac50834ad6b13b1f3480e1d
Nik <8700286+ntextreme3@users.noreply.github.com>
2025-01-16 08:59:46 -0500

    fix install script, now that helpers are ignored

--- run_once_before_install-tools.sh
+++ run_once_before_install-tools.sh
@@ -6,13 +6,13 @@ sudo apt update
 sudo apt install -y build-essential cmake
 
 # install things with apt
-missing_apts=$(python3 _find_missing_packages.py tmux rg::ripgrep jq rustup tree)
+missing_apts=$(python3 ${CHEZMOI_SOURCE_DIR}/_find_missing_packages.py tmux rg::ripgrep jq rustup tree)
 if [[ ! -z $missing_apts ]]; then
     sudo apt install -y $missing_apts
 fi
 
 # install things with snap
-missing_snaps=$(python3 _find_missing_packages.py jqp yq)
+missing_snaps=$(python3 ${CHEZMOI_SOURCE_DIR}/_find_missing_packages.py jqp yq)
 if [[ ! -z $missing_snaps ]]; then
     sudo snap install $missing_snaps
 fi
