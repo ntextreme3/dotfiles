964635c464adbefd0c88d676a05d8e28868b22fd
Nik Trenchi <ntrenchi@bloomberg.net>
2025-07-07 11:11:25 -0400

    set required tmux env
    Alternatively, can update our instructions here to require running in
    tmux, but that doesn't seem reasonable since tmux may be installed by
    chezmoi.

--- .chezmoiscripts/run_once_after_tmux-tpm.sh
+++ .chezmoiscripts/run_once_after_tmux-tpm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+# This would normally be set inside a tmux session.
+export TMUX_PLUGIN_MANAGER_PATH=/home/ntrenchi/.tmux/plugins/
+
 # Install tmux plugins via tpm (alternative is prefix+I in tmux)
 ~/.tmux/plugins/tpm/bin/install_plugins
