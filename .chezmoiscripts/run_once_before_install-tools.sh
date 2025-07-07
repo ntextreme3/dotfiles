8821c1decc56f47321c3f6fc67007b9376f44a6f
Nik Trenchi <ntrenchi@bloomberg.net>
2025-07-07 11:01:02 -0400

    move bash-completion to a dependency
    not sure if it's worth distinguishing here, but whatever

--- .chezmoiscripts/run_once_before_install-tools.sh
+++ .chezmoiscripts/run_once_before_install-tools.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 # install dependencies
 sudo apt update
-sudo apt install -y --no-upgrade build-essential cmake
+sudo apt install -y --no-upgrade build-essential cmake bash-completion
 
 # install tools with apt
-sudo apt install -y --no-upgrade curl git tmux ripgrep jq rustup tree kubectx fzf bash-completion
+sudo apt install -y --no-upgrade curl git tmux ripgrep jq rustup tree kubectx fzf
 
 # install tools with snap
 sudo snap install jqp
