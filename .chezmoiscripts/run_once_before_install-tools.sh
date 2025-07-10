804b30f12adf4ad8e0720b60cfb74244c356d565
Nik Trenchi <ntrenchi@bloomberg.net>
2025-07-10 16:06:09 -0400

    install net-tools

--- .chezmoiscripts/run_once_before_install-tools.sh
+++ .chezmoiscripts/run_once_before_install-tools.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt install -y --no-upgrade build-essential cmake bash-completion
 
 # install tools with apt
-sudo apt install -y --no-upgrade curl git tmux ripgrep jq rustup tree kubectx
+sudo apt install -y --no-upgrade curl git tmux ripgrep jq rustup tree kubectx net-tools
 
 # install tools with snap
 sudo snap install jqp
