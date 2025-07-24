2a3b3cc15c50eebe1bb7c63469eccc066ea2e36d
Nik <8700286+ntextreme3@users.noreply.github.com>
2025-07-24 11:05:18 -0400

    overwrite symlinks to be idempotent
    otherwise, this fails after the first run because it's already there.

--- .chezmoiscripts/run_once_after_create-symlinks.sh
+++ .chezmoiscripts/run_once_after_create-symlinks.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 # `bat` via apt is installed as `batcat` due to a name conflict
-sudo ln -s "$(command -v batcat)" ~/.local/bin/bat
+sudo ln -sf "$(command -v batcat)" ~/.local/bin/bat
