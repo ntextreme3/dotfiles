b4a217aac9e9ecf1a66adc113b5893f0ecba8b41
Nik Trenchi <ntrenchi@bloomberg.net>
2025-12-10 11:03:18 -0500

    skip go PATH change if go is not installed

--- dot_bashrc.d/base/00-env.sh
+++ dot_bashrc.d/base/00-env.sh
@@ -7,5 +7,7 @@ export GPG_TTY=$(tty)
 # Includes starship and other crates
 PATH=$HOME/.cargo/bin:$PATH
 
-# Include Go binaries
-PATH=$(go env GOPATH)/bin:$PATH
+# Include Go binaries if `go` is installed
+if command -v go &> /dev/null; then
+    PATH=$(go env GOPATH)/bin:$PATH
+fi
